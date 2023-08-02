1a04dd13f6e21d42e53809430cdffb6898e5466c
HK116 <henokmekonenpl@gmail.com>
2023-08-02 11:42:29 +0200

    Aligh Movie card center

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -11,14 +11,14 @@ const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
     <div className="group bg-zinc-900 col-span relative h-[12vw]">
       <img
         className="cursor-pointer object-cover transition duration shadow-xl group-hover:opacity-90
-                    sm:group-hover:opacity-0 delay-300 w-full h-[12vw]"
+                    sm:group-hover:opacity-0 delay-200 w-full h-[12vw]"
         src={data.thumbnailUrl}
         alt="Thumbnail"
       />
       <div
-        className="opacity-0 absolute top-0 transition duration-200 z-10 invisible sm:visible
-                        delay-300 w-full scale-0 group-hover:scale-110 group-hover:-translate-y-[6vw]
-                        group-hover:translate-x-[2vw] group-hover:opacity-100"
+        className="opacity-0 absolute top-0 transition duration-100 z-10 invisible sm:visible
+                        delay-200 w-full scale-0 group-hover:scale-110 group-hover:-translate-y-[6vw]
+                        group-hover:opacity-100 shadow-xl"
       >
         <img
           className="cursor-pointer object-conver transition duration shadow-xl w-full h-[12vw]"
